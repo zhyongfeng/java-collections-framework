720c033c4ce3d783eaf32083b1bd36f53b1758c7
= <zhangyongfeng@jd.com>
2018-08-21 09:21:27 +0800

    #1 for list

--- src/list/ListMain.java
+++ src/list/ListMain.java
@@ -1,6 +1,7 @@
 package list;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ListMain {
@@ -23,6 +24,26 @@ public class ListMain {
         for (int i = 0; i < numbers.size() ; i++) {
             System.out.println(numbers.get(i));
         }
+
+        System.out.println("\nIteration #3");
+
+        // iterator
+        List<String> values = new ArrayList<String>();
+        values.add("hello, ");
+        values.add("this ");
+        values.add("is ");
+        values.add("a list");
+
+        Iterator iterator = values.iterator();
+        while (iterator.hasNext()){
+            String next = String.valueOf(iterator.next());
+            System.out.println(next);
+        }
+
+        // removing items (careful!)
+        // this is very slow, especially you have a big arraylist
+        // as arraylist moves all the other items below index 0.
+        numbers.remove(0);
 
     }
 }
