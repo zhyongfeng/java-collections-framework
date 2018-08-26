39fff7821cc1392e4dd7ae911f157f853e22829f
zhyongfeng <zyf0808@yeah.net>
2018-08-26 17:04:35 +0800

    test of retainAll method.

--- src/list/ArrayListMain.java
+++ src/list/ArrayListMain.java
@@ -14,8 +14,7 @@ public class ArrayListMain {
 
         System.out.println("\nIteration #1");
 
-        for (Integer i : numbers
-                ) {
+        for (Integer i : numbers) {
             System.out.println(i);
         }
 
@@ -44,6 +43,26 @@ public class ArrayListMain {
         // this is very slow, especially you have a big arraylist
         // as arraylist moves all the other items below index 0.
         numbers.remove(0);
+
+        List<String> stringList1 = new ArrayList<String>();
+        stringList1.add("a");
+        stringList1.add("b");
+        stringList1.add("c");
+
+        List<String> stringList2 = new ArrayList<String>();
+        stringList2.add("a");
+        stringList2.add("b");
+        stringList2.add("d");
+
+        //note: if it's a list of Integer, retainAll will make it empty, due to Integer is an object
+        System.out.println(stringList1.retainAll(stringList2));
+
+        Iterator it = stringList1.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next().toString());
+        }
+
+
 
     }
 }
