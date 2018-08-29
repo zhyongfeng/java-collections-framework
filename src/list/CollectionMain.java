40e4ab8e89402a92062973dd9138f75bdfc3011d
= <zhangyongfeng@jd.com>
2018-08-29 19:23:03 +0800

    explain add/addAll/retainAll/contains/containAll/size method in a collection

--- src/list/CollectionMain.java
+++ src/list/CollectionMain.java
@@ -2,10 +2,7 @@ package list;
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import javax.swing.text.html.HTMLDocument;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 /*add() adds the given element to the collection,
 and returns true if the Collection changed as a result of calling
@@ -33,5 +30,41 @@ public class CollectionMain {
         while (((Iterator) it).hasNext()){
             System.out.println(it.next().toString());
         }
+
+
+        Collection colA = new ArrayList();
+        ((ArrayList) colA).add("A");
+        ((ArrayList) colA).add("B");
+        ((ArrayList) colA).add("C");
+
+        Collection colB = new ArrayList();
+        ((ArrayList) colB).add(1);
+        ((ArrayList) colB).add(2);
+        ((ArrayList) colB).add(3);
+
+        Collection target = new ArrayList();
+
+        target.addAll(colA);     //target now contains [A,B,C]
+        System.out.println(target);
+        target.addAll(colB);     //target now contains [A,B,C,1,2,3]
+        System.out.println(target);
+        target.retainAll(colB);  //target now contains [1,2,3]
+        System.out.println(target);
+        target.removeAll(colA);  //nothing happens - already removed
+        System.out.println(target);
+        target.removeAll(colB);  //target is now empty.
+        System.out.println(target);
+
+
+        Collection collection2   = new HashSet();
+        boolean containsElement = collection2.contains("an element");
+
+        //return true if both the collections are empty
+        Collection elements     = new HashSet();
+        boolean containsAll     = collection2.containsAll(elements);
+
+        System.out.println(containsElement);
+        System.out.println(containsAll);
+
     }
 }
